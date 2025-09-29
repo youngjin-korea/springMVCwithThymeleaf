e3a98f610a26eb0dda8b569f2ae309c9b682b276
YoungJinKim <youngjin1293@naver.com>
2025-09-29 19:26:41 +0900

    firstDayClear

---
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@Slf4j
+@RestController
+public class RequestHeaderController {
+    @RequestMapping("/headers")
+    public String headers (
+            HttpServletRequest request,
+            HttpServletResponse response,
+            HttpMethod method,
+            Locale locale,
+            @RequestHeader MultiValueMap<String, String> headerMap,
+            @RequestHeader("host") String host,
+            @CookieValue(value = "myCookie", required = false) String cookie
+            ) {
+        return "ok";
+    }
+}
