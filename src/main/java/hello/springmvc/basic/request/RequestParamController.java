d0944ce868854339313b783af2ead1ee9f63cb31
YoungJinKim <youngjin1293@naver.com>
2025-09-30 14:55:46 +0900

    250930controllerMapping

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
@@ -14,11 +16,32 @@ import java.io.IOException;
 public class RequestParamController {
 
     @RequestMapping("/request-param-v1")
-    public void requestParamV1 (HttpServletRequest request, HttpServletResponse response, @CookieValue(value = "savedId", required = false) String savedId) throws IOException {
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response, @CookieValue(value = "savedId", required = false) String savedId) throws IOException {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
         log.info("username={}, age={} cookie = {}", username, age, savedId);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody // 뷰리졸버 호출 없이 messagebody에 넣음
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam(value = "username", required = false) String memberName, @RequestParam(value = "age", required = true, defaultValue = "12") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "OK";
+    }
+
+    @ResponseBody // 뷰리졸버 호출 없이 messagebody에 넣음
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username, @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody // 뷰리졸버 호출 없이 messagebody에 넣음
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "OK";
     }
 }
